af3a495a7a426d42ed3d93ef181b291a09c4c0e7
Fernado Silvosa <fernando.silvosa@gmail.com>
2021-11-03 14:54:52 +0100

    Creados 2 fragments para el login e interconectados.

--- app/src/main/java/com/example/beers_of_the_world/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/beers_of_the_world/ui/fragments/LoginFragment.kt
@@ -1,21 +1,26 @@
-/*
- * Copyright (c) FS
- */
-
-
 
 package com.example.beers_of_the_world.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
 import com.example.beers_of_the_world.R
+import com.example.beers_of_the_world.databinding.FragmentLoginBinding
+import com.example.beers_of_the_world.databinding.FragmentMainBinding
+import com.example.beers_of_the_world.ui.activities.MainActivity
+import com.example.beers_of_the_world.viewModel.LoginViewModel
 
 
 class loginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var navController: NavController
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +35,38 @@ class loginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*subscribeUI()*/
+
+        buildListeners()
+
 
     }
+
+    private fun buildListeners() {
+        buttonPassListener()
+    }
+
+    private fun buttonPassListener() {
+        passToSecondAct()
+    }
+
+    private fun passToSecondAct() {
+        binding.buttonpass.setOnClickListener {
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
+    }
+
+    /*private fun bLoginListener() {
+        binding.buttonpass.setOnClickListener {
+            //TODO GET PREFIX
+            getToken()
+        }
+    }*/
+
+    /*private fun getToken() {
+        ViewModel.getToken()
+    }*/
 }
---
+++ app/src/main/java/com/example/beers_of_the_world/viewModel/LoginViewModel.java
@@ -0,0 +1,12 @@
+
+
+package com.example.beers_of_the_world.viewModel;
+
+public class LoginViewModel {
+
+
+
+    /*getToken() {
+
+    }*/
+}
